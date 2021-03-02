9541a7df9f838d8c0747a9de53dc966c0068858b
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-02 23:49:31 +0000

    adicionado questões 0,1,2

---
+++ SEMANA01_q00.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  int a=2;
+  int b=3;
+    printf("%i %i\n",a,b);
+  int c=3;
+  int d=2;
+    printf("%i %i\n",d,c);
+  int e=2;
+  int f=2;
+    printf("%i %i\n",e,f);
+  return 0;
+  }
+  */
---
+++ SEMANA01_q02.c
@@ -0,0 +1,10 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  int a,b;
+  scanf("%i %i",&a,&b);
+  printf("%i %i %i %i %i %i\n",a-1,a,a+1,b-1,b,b+1);
+  return 0;
+  }
+  */
