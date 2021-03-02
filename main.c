76729e81f504afc30ede80efdae878a30b1211e9
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-02 23:38:49 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  float valor;
+  float a;
+  float b;
+  float c;
+  printf("digite o valor\n");
+  scanf("%f",&valor);
+  if(valor <= 100.00){ 
+    a = valor;
+    printf("R$ %.2f\n", valor); 
+  }else{
+    if (valor >= 200.00){
+    c = valor - (valor*0.1);
+    printf("R$ %.2f\n", c);
+  }else{
+    b = valor - (valor*0.05);
+    printf("R$ %.2f\n", b);
+    }
+  }
+  return 0;
+}
