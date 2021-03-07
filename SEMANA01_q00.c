065e5f32b9394e9cf3bfbc19d4f3093e3e725733
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-07 00:01:35 +0000

    Edição da questão 01_q00

--- SEMANA01_q00.c
+++ SEMANA01_q00.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 int main(void) {
-  int a=2;
-  int b=3;
+  int a,b;
+  scanf("%i %i",&a,&b);
+  if  (a<=b) {
     printf("%i %i\n",a,b);
-  int c=3;
-  int d=2;
-    printf("%i %i\n",d,c);
-  int e=2;
-  int f=2;
-    printf("%i %i\n",e,f);
+  }else{
+    printf("%i %i\n",b,a);
+  }
   return 0;
   }
   */
+  
--- SEMANA02_q05.c
+++ SEMANA02_q05.c
@@ -1,7 +1,23 @@
+/*
 #include <stdio.h>
 
 int main(void) {
+  int a,b;
+  scanf("%i %i",&a,&b);
   
-    
+  if (a-b>=0){
+    printf("%i %i %i %i %i %i\n",a-1,a,a+1,b-1,b,b+1);
+  }else{
+    if (a-b<=1){
+      printf("%i %i %i %i %i %i\n",a-2,a,a+2,b-2,b,b+2);
+    }else{
+      if(a-b<=2){
+        printf("%i %i %i %i %i %i\n",a-1,a,a+1,b-1,b,b+1);
+      }else{
+      printf("%i %i %i %i %i %i\n",a-1,a,a+1,b-1,b,b+1);
+      }
+    }
+  }
   return 0;
 }
+*/
