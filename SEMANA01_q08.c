471f3fca651c754e306c02f53b7df4e83439965e
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-24 18:56:05 +0000

    questão 8

---
+++ SEMANA01_q08.c
@@ -0,0 +1,24 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  float x,y;
+  scanf("%f\n %f", &x, &y);
+
+  if  (x-11>58 && x+11<98 && y-11>204 && y+11<244){
+    printf("GOLACO");
+  }else{
+    if  (x-11>750 && x+11<790 && y-11>204 && y+11<244){
+    printf("GOLACO");
+    }else{
+      if  (x>=790 || x<=58 || y>=244){
+    printf("UHHH");
+    }else{
+      printf("GOL");
+    }
+  }
+  }
+    
+  return 0;
+}
+*/
