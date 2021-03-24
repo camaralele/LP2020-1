36944e6bd461253f675d052425ac8505ba37be38
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-24 19:28:51 +0000

    questão 9

---
+++ SEMANA01_q09.c
@@ -0,0 +1,46 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  int valor, qtd100, qtd50, qtd20, qtd10, qtd5, qtd2, qtd1,resto;
+  scanf("%i", &valor);
+
+  qtd100=valor/100;
+  resto=valor%100;
+  if  (qtd100>0){
+    printf("%i notas de R$ 100\n",qtd100);
+  }
+  qtd50=resto/50;
+  resto=resto%50;
+  if (qtd50>0){
+    printf("%i notas de R$ 50\n",qtd50);
+  }
+  qtd20=resto/20;
+  resto=resto%20;
+  if (qtd20>0){
+    printf("%i notas de R$ 20\n",qtd20);
+  }
+  qtd10=resto/10;
+  resto=resto%10;
+  if (qtd10>0){
+    printf("%i notas de R$ 10\n",qtd10);
+  }
+  qtd5=resto/5;
+  resto=resto%5;
+  if (qtd5>0){
+    printf("%i notas de R$ 5\n",qtd5);
+  }
+  qtd2=resto/2;
+  resto=resto%2;
+  if (qtd2>0){
+    printf("%i notas de R$ 2\n",qtd2);
+  }
+  qtd1=resto/1;
+  resto=resto%1;
+  if (qtd1>0){
+    printf("%i notas de R$ 1\n",qtd1);
+  }
+ 
+  return 0;
+}
+*/
