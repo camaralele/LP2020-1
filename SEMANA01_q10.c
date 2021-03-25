3b3c9b6cecc24c4630a42d9cd11fa520370d15e5
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-25 23:02:13 +0000

    questão 10

---
+++ SEMANA01_q10.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+int main(void) {
+  int num, resto, milhar, centena, dezena, unidade;
+  scanf("%i", &num);
+  
+  milhar=num/1000;
+  resto=num%1000;
+  centena=resto/100;
+  resto=resto%100;
+  dezena=resto/10;
+  resto=resto%10;
+  unidade=resto/1;
+
+
+  switch (milhar){
+    case 1: printf("Mil ");
+    break;
+    case 2: printf("Dois mil ");
+    break;
+    case 3: printf("Três mil ");
+    break;
+    case 4: printf("Quatro mil ");
+    break;
+    case 5: printf("Cinco mil ");
+    break;
+    case 6: printf("Seis mil ");
+    break;
+    case 7: printf("Sete mil ");
+    break;
+    case 8: printf("Oito mil ");
+    break;
+    case 9: printf("Nove mil ");
+    break;
+    default: printf("");
+    break;
+  }
+
+ switch (centena){
+    case 1: printf("Cem ");
+    break;
+    case 2: printf("Duzentos ");
+    break;
+    case 3: printf("Trezentos ");
+    break;
+    case 4: printf("Quatrocentos ");
+    break;
+    case 5: printf("Quinhentos ");
+    break;
+    case 6: printf("Seiscentos ");
+    break;
+    case 7: printf("Setecentos ");
+    break;
+    case 8: printf("Oitocentos ");
+    break;
+    case 9: printf("Novecentos ");
+    break;
+    default: printf("");
+    break;
+  }
+
+ if (milhar !=0 || centena !=0) {
+   printf(" e ");
+ }
+
+switch (dezena){
+    case 1: switch (unidade){
+      case 1: printf ("onze");
+      break;
+      case 2: printf ("doze");
+      break;
+      case 3: printf ("treze");
+      break;
+      case 4: printf ("quatorze");
+      break;
+      case 5: printf ("quinze");
+      break;
+      case 6: printf ("dezesseis");
+      break;
+      case 7: printf ("dezessete");
+      break;
+      case 8: printf ("dezoito");
+      break;
+      case 9: printf ("dezenove");
+      break;
+      default: printf ("");
+      break;
+    }
+    break;
+    case 2: printf("Vinte ");
+    break;
+    case 3: printf("Trinta ");
+    break; 
+    case 4: printf("Quarenta ");
+    break;
+    case 5: printf("Cinquenta ");
+    break;
+    case 6: printf("Sessenta ");
+    break;
+    case 7: printf("Setenta ");
+    break;
+    case 8: printf("Oitenta ");
+    break;
+    case 9: printf("Noventa ");
+    break;
+    default: printf("");
+    break;
+  }
+
+  if (dezena !=0) {
+   printf(" e ");
+ }
+
+if  (dezena !=1){
+  switch (unidade){
+    case 1: printf("Um");
+    break;
+    case 2: printf("dois");
+    break;
+    case 3: printf("Três");
+    break;
+    case 4: printf("Quatro");
+    break;
+    case 5: printf("Cinco");
+    break;
+    case 6: printf("Seis");
+    break;
+    case 7: printf("Sete");
+    break;
+    case 8: printf("Oito");
+    break;
+    case 9: printf("Nove");
+    break;
+    default: printf("");
+    break;
+  }
+ }
+  
+return 0;
+}
