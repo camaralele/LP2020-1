2f2f3fd3a7b04c3db29e4d37a512131ebf82cf9d
Letícia Padilha <leticiacamara93@acad.charqueadas.ifsul.edu.br>
2021-03-28 13:49:29 +0000

    questão 11

--- SEMANA01_q11.c
+++ SEMANA01_q11.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main(void) {
-  int impar1,impar2,par,passo4,passo5,cod1,cod2,cod3,cod4,cod5,cod6,cod7,cod8,cod9,cod10,cod11,cod12,cod13;
+  int passo1,passo2,passo3,passo4,passo5,cod1,cod2,cod3,cod4,cod5,cod6,cod7,cod8,cod9,cod10,cod11,cod12,cod13;
   
   scanf("%i %i %i %i %i %i %i %i %i %i %i %i %i", &cod1,&cod2,&cod3,&cod4,&cod5,&cod6,&cod7,&cod8,&cod9,&cod10,&cod11,&cod12,&cod13);
 
- impar1 = cod2 + cod4 + cod6 + cod8 + cod10 + cod12;
- impar2 = impar1 * 3;
- par = cod3 + cod5 + cod7 + cod9 + cod11 + cod13;
- passo4 = impar2 + par;
+ passo1 = cod2 + cod4 + cod6 + cod8 + cod10 + cod12;
+ passo2 = passo1 * 3;
+ passo3 = cod3 + cod5 + cod7 + cod9 + cod11 + cod13;
+ passo4 = passo2 + passo3;
  passo5 = passo4 + cod1;
 
  if (passo5 % 10 == 0){
