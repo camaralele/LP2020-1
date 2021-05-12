68f6012b1d66591594d947313a540b7e24f1e404
Letícia Padilha <>
2021-05-12 21:59:53 +0000

    questão 16

--- SEMANA09_q16.c
+++ SEMANA09_q16.c
@@ -19,15 +19,16 @@ int main(void) {
     x1 = novo;
 
   resultado = (x1+x2+x3+x4+x5)/5.0;
-  printf ("%f \n",resultado);
+  
+  if (resultado>x){
+    printf("DEU RUIM\n");
+    break;
+  }
 
   }while (novo>=0);
   
   if  (resultado<=x){
       printf("TURNO TRANQUILO\n");
-    }
-  if  (resultado>x){
-      printf("DEU RUIM\n");
     }
 
   return 0;
