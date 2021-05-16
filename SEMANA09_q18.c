885dbf83b80e91a0dbbcbe32701479670d1d2b2f
Letícia Padilha <>
2021-05-16 22:08:08 +0000

    questão 18

---
+++ SEMANA09_q18.c
@@ -0,0 +1,18 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  int i, t=1000, num[t];
+    
+  scanf ("%i", &t);
+  for(i = 0; i < t; i++) {
+    scanf("%d", &num[i]);
+  }
+
+  for (i=t-1; i>=0; i--){
+    printf  ("%i ", num[i]);
+ }
+
+  return 0;
+}
+*/
---
+++ SEMANA09_q19.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  char c, t, d, s, i;
+  int n [100], linha, coluna;
+  
+  for (linha = 0; linha <n; linha ++){
+    scanf ()
+    for (coluna =0; coluna <n; coluna ++){
+      printf("%i %i", linha, coluna);
+    }
+    printf ("\n");
+  }
+  return 0;
+}
+*/
