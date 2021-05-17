879d7dfe0e81d363147a040b2c9a83368bf60599
Letícia Padilha <>
2021-05-17 01:40:25 +0000

    questão 21

---
+++ SEMANA09_q21.c
@@ -0,0 +1,23 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  int vet[100], i, maior, cont=1;
+  
+  for(i = 0; i < 100; i++) {
+    scanf("%i", &vet[i]);
+  }
+  for(i=0;i<100;i++){
+    if (vet[i] == vet[i+1]){
+      cont++;
+      if (cont>maior){
+     	maior = cont;  
+      }
+ 	}else{
+    	cont=1;
+  	}
+  }
+  printf ("%i", maior);
+  return 0;
+}
+*/
