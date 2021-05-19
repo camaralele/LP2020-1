dcf4b7b554fa56604b4c9db446d1e97f48f6be9e
Letícia Padilha <>
2021-05-19 18:46:44 +0000

    Questão 19

--- SEMANA09_q19.c
+++ SEMANA09_q19.c
@@ -2,16 +2,69 @@
 #include <stdio.h>
 
 int main(void) {
-  char c, t, d, s, i;
-  int n [100], linha, coluna;
-  
-  for (linha = 0; linha <n; linha ++){
-    scanf ()
-    for (coluna =0; coluna <n; coluna ++){
-      printf("%i %i", linha, coluna);
-    }
-    printf ("\n");
-  }
+  int num, linha=0, coluna=0, matriz[num][num];
+	char letra;
+	
+	scanf("%i %c", &num, &letra);
+	
+	printf("%i \n", num);
+
+
+	if(letra == 'T'){
+		for (linha=0; linha<num; linha++){
+			    for (coluna=0; coluna<num; coluna++){
+            if ((linha+coluna)%2==0){
+              printf ("1");
+            }else{
+              printf ("0");
+            }
+			  }
+        printf ("\n");
+ 			}
+		}
+
+  if(letra == 'I'){
+		for (linha=0; linha<num; linha++){
+			    for (coluna=0; coluna<num; coluna++){
+            if (coluna <= linha){
+              matriz [linha][coluna]=1;
+            }else{
+              matriz [linha][coluna]=0;
+            }
+            printf ("%i", matriz [linha] [coluna]);
+			  }
+        printf ("\n");
+ 			}
+		}
+
+  if(letra == 'D'){
+		for (linha=0; linha<num; linha++){
+			    for (coluna=0; coluna<num; coluna++){
+            if (coluna == linha){
+              matriz [linha][coluna]=1;
+            }else{
+              matriz [linha][coluna]=0;
+            }
+            printf ("%i", matriz [linha] [coluna]);
+			  }
+        printf ("\n");
+ 			}
+		}
+
+  if(letra == 'S'){
+		for (linha=0; linha<num; linha++){
+			    for (coluna=0; coluna<num; coluna++){
+            if (coluna >= linha){
+              matriz [linha][coluna]=1;
+            }else{
+              matriz [linha][coluna]=0;
+            }
+            printf ("%i", matriz [linha] [coluna]);
+			  }
+        printf ("\n");
+ 			}
+		}
+
   return 0;
 }
 */
---
+++ SEMANA09_q23.c
@@ -0,0 +1,46 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+  char carta[1000];
+  int i, player1=0, player2=0;
+
+  do {
+    scanf ("%c", &carta[i]);
+
+    
+  switch (i){
+    case 'K': i=13; // rei
+    break;
+    case 'Q': i=12; // dama
+    break;
+    case 'J': i=11; // valete
+    break;
+    case '10': i=10;
+    break;
+    case '9': i=9;
+    break;
+    case '8': i=8;
+    break;
+    case '7': i=7;
+    break;
+    case '6': i=6;
+    break;
+    case '5': i=5;
+    break;
+    case '4': i=4;
+    break;
+    case '3': i=3;
+    break;
+    case '2': i=2;
+    break;
+    case 'A': i=1; // as 
+    break;
+    case 'S': break; // parada
+  }
+  }
+
+
+  return 0;
+}
+*/
